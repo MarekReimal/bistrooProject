d59fb8e58aee4a2c75be8477db913a20650f357b
MarekReimal <marekreimal@gmail.com>
2024-01-07 23:53:00 +0200

    public vaade ilusaks teha

--- bistrooapp_public/views.py
+++ bistrooapp_public/views.py
@@ -17,14 +17,14 @@ def show_menu(request):
 
     # Hinna töötlus, kirjutab "Prae hinna sees"
     for item in q_result_menuu:
-        # Kui hind on 0 või 0 ja none
-        if ((str(item["price_full"]) == "0.00" and str(item["price_half"]) == "0.00") or
+        # Kui täis ja pool hind on olemas siis näitab mõlemat
+        if str(item["price_full"]) > "0.00" and (str(item["price_half"]) > "0.00" and str(item["price_half"]) != "None"):
+            item["price_full"] = str(item["price_full"]) + " / " + str(item["price_half"])
+        # Kui täishind on 0 ja poolhind 0 ja none siis näitab prae hinna sees
+        elif ((str(item["price_full"]) == "0.00" and str(item["price_half"]) == "0.00") or
                 (str(item["price_full"]) == "0.00" and str(item["price_half"]) == "None")):
-            # siis väärtus ...
             item["price_full"] = "Prae hinna sees"
 
-        if str(item["price_half"]) == "0.00" or str(item["price_half"]) == "None":
-            item["price_half"] = ""
 
     context = {
         "formatted_date": formatted_date,
